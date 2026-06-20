c34c495670decaea089d540c99b3c80b7b0f20e6
Rajat Jain <rajatjain4294@gmail.com>
2026-06-20 13:08:02 +0530

    subnet addded

---
+++ azurerm_subnet/main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_subnet" "subnet1" {
+    for_each = var.subnets_LZ
+    name = each.value.name
+    location = each.value.location
+    resource_group_name = each.value.rg_name
+    virtual_network_name = each.value.virtual_network_name
+    address_prefixes = each.value.address_prefix
+}
